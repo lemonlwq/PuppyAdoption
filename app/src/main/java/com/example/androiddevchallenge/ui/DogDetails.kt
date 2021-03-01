0c83cba7a24245ee9a2c77f8bf2b8707e29a9e32
MLinWQ <lwqweiqiang-1683@qq.com>
2021-03-01 16:28:55 +0800

    fix lint error(check 4)

--- app/src/main/java/com/example/androiddevchallenge/ui/DogDetails.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/DogDetails.kt
@@ -22,11 +22,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
